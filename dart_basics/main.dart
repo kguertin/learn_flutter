d4c265d220c89e5713be664df965b348ae87c422
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-17 19:49:55 -0400

    dart names parameters and class constructors

--- dart_basics/main.dart
+++ dart_basics/main.dart
@@ -1,17 +1,18 @@
 class Person {
-  String name = 'Kevin';
-  int age = 31;
+  String? name; 
+  int? age;
+
+  Person({this.name, this.age });
 }
+
 
 double addNumbers(double num1, double num2){
   return num1 + num2;
 }
  
 void main () {
-  var p1 = Person();
-  var p2 = Person();
-  p2.name = "Sally";
-  p2.age = 28;
+  var p1 = Person(name: "Kevin", age: 31);
+  var p2 = Person(name: "Sally", age: 28);
   print("Name: ${p1.name} \nAge: ${p1.age}");
   print("Name: ${p2.name} \nAge: ${p2.age}");
   double firstResult;
