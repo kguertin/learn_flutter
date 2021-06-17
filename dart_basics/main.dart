cfc91b97d2d5aa356d883ac38b9d25d34bc929f5
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-17 18:20:14 -0400

    more dart basics

--- dart_basics/main.dart
+++ dart_basics/main.dart
@@ -1,8 +1,20 @@
+class Person {
+  String name = 'Kevin';
+  int age = 31;
+}
+
 double addNumbers(double num1, double num2){
   return num1 + num2;
 }
-
+ 
 void main () {
-  print(addNumbers(1, 2.6));
-  print(addNumbers(1, 1));
+  var p1 = Person();
+  var p2 = Person();
+  p2.name = "Sally";
+  p2.age = 28;
+  print("Name: ${p1.name} \nAge: ${p1.age}");
+  print("Name: ${p2.name} \nAge: ${p2.age}");
+  double firstResult;
+  firstResult = addNumbers(1, 1);
+  print(firstResult + 1.5);
 }
