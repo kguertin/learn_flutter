a15aa80d4b146968696ee787212da72a447c2355
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-18 18:33:31 -0400

    flutter syntax shortcuts

--- dart_basics/main.dart
+++ dart_basics/main.dart
@@ -13,8 +13,10 @@ double addNumbers(double num1, double num2){
 void main () {
   var p1 = Person(name: "Kevin", age: 31);
   var p2 = Person(name: "Sally", age: 28);
+  
   print("Name: ${p1.name} \nAge: ${p1.age}");
   print("Name: ${p2.name} \nAge: ${p2.age}");
+   
   double firstResult;
   firstResult = addNumbers(1, 1);
   print(firstResult + 1.5);
