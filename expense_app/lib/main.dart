500f6194dfc8987b11527684267dbd5f39a54010
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-22 13:43:17 -0400

    mapping data to widgets

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'New shoes', amount: 69.99, date: DateTime.now()), 
+    Transaction(id: 't2', title: 'Weekly Groceries', amount: 16.54, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +37,9 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text("List of TX"),
-            color: Colors.red,
-          ),
+         Column(children: transactions.map((tx){
+           return Card(child: Text(tx.title),);
+         }).toList(),),
         ],
       ),
     );
