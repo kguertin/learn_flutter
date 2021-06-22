8d1c8b0b5cbe42e8ccc2822b1da76acb9e95592e
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-22 14:24:49 -0400

    style list item component tree

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -41,12 +41,34 @@ class MyHomePage extends StatelessWidget {
            return Card(child: Row(
              children: <Widget>[
                Container(
-                 child: Text(tx.amount.toString())
+                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                 decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
+                 padding: EdgeInsets.all(10),
+                 child: Text(tx.amount.toString(), style: TextStyle(
+                   fontWeight: FontWeight.bold, 
+                   fontSize: 20, 
+                   color: Colors.purple)
+                   )
                  ),
-                 Column(children: [
-                   Text(tx.title),
-                   Text(tx.date.toString())
-                 ],)
+                 Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                    Text(
+                      tx.title, 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        ),
+                      ),
+                    Text(
+                      tx.date.toString(),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
             );
