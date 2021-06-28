1b8573b42a690ed4f7c6d668c14a0043cd03575a
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-28 15:10:31 -0400

    add conditional styles based on device size and orientation

--- expense_app/lib/widgets/transactionList.dart
+++ expense_app/lib/widgets/transactionList.dart
@@ -53,11 +53,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    onPressed: () => deleteTx(transactions[index].id),
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? FlatButton.icon(
+                          textColor: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                          icon: Icon(Icons.delete),
+                          label: Text("Delete"),
+                        )
+                      : IconButton(
+                          onPressed: () => deleteTx(transactions[index].id),
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                        ),
                 ),
               );
             },
