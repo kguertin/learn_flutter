ec469757d13d8a2e44c05f58dc22d0654a25b6a4
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-09-19 17:01:45 -0400

    create category item widget

--- meal_app/lib/categories_screen.dart
+++ meal_app/lib/categories_screen.dart
@@ -1,15 +1,23 @@
 import "package:flutter/material.dart";
+import 'package:flutter_complete_guide/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      children: DUMMY_CATEGORIES
+          .map((categoryData) => CategoryItem(
+                categoryData.title,
+                categoryData.color,
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20),
-      children: <Widget>[],
     );
   }
 }
